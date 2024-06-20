72cfbe17acd9ba45b4438c2667de36f877591d4d
Eternal-could <20578466@qq.com>
2024-06-20 18:26:06 +0800

    feat: add playground

---
+++ playground/src/main.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// // import "./index.css";
+//
+// ReactDOM.render(<App />, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => <div>hello 123123</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// @ts-ignore
+import.meta.hot.accept(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
